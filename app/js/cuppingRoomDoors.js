4568e3a63d8cc860d0732056c9f7aba3fa4897d4
Brian Hampson (PSP) <bhampson@starbucks.com>
2017-04-13 17:32:59 -0700

    Adding functionality to playback text and image items

--- app/js/cuppingRoomDoors.js
+++ app/js/cuppingRoomDoors.js
@@ -4,6 +4,22 @@ const ipcRenderer = electron.ipcRenderer;
 console.log('render thread');
 
 let videoPlayer_ = null;
+let fullScreenText = document.getElementById('fullScreenText');
+let fullScreenImage = document.getElementById('fullScreenImage');
+let video = document.getElementById('fullScreenText');
+
+fullScreenText.addEventListener("transitionend", function(event) {
+    fullScreenText.className = '';
+}, false);
+
+fullScreenImage.addEventListener("transitionend", function(event) {
+    fullScreenImage.className = '';
+}, false);
+
+fullScreenImage.addEventListener('load', function()  { 
+    fullScreenText.className = 'fadeIn';
+    fullScreenImage.className = 'fadeIn';
+}, false);
 
 window.addEventListener('DOMContentLoaded', init);
 
@@ -36,33 +52,33 @@ function videoError(error) {
     //Do something
 }
 
-            // "id" : this._id,
-            // "text" : this._text,
-            // "localPath" : this._localPath,
-            // "activeStartDate" : this._activeStartDate,
-            // "activeEndDate" : this._activeEndDate,
-            // "durationMs" : this.durationMs,
-            // "type" : this._type,
-            // "target" : this._target,
-            // "mediaType" : this.mediaType
+// "id" : this._id,
+// "text" : this._text,
+// "localPath" : this._localPath,
+// "activeStartDate" : this._activeStartDate,
+// "activeEndDate" : this._activeEndDate,
+// "durationMs" : this.durationMs,
+// "type" : this._type,
+// "target" : this._target,
+// "mediaType" : this.mediaType
 
 function play(playbackItem) {
     try {
         if (playbackItem.mediaType) {
             switch(playbackItem.mediaType) {
-                case 'text': {
-                    let fullScreenText = document.getElementById('fullScreenText');
-                    fullScreenText.innerHTML = playbackItem.text;
-                    break;
-                }
                 case 'image': {
+                    fullScreenImage.src = playbackItem.localPath;
+                    fullScreenText.innerHTML = playbackItem.text;
                     break;
                 }
                 case 'video': {
                     break;
                 }
+                default: {
+                    break;
+                }
             }
-       }
+        }
     } catch (error) {
         console.log(error);
     }
@@ -89,3 +105,30 @@ function removeClass(selector, myClass) {
     elements[i].classList.remove(myClass);
   }
 }
+
+// function fadeIn(element) {
+//     var op = 0.1;  // initial opacity
+//     element.style.display = 'block';
+//     var timer = setInterval(function () {
+//         if (op >= 1){
+//             clearInterval(timer);
+//         }
+//         element.style.opacity = op;
+//         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+//         op += op * 0.1;
+//     }, 10);
+// }
+
+// function fadeOut(element) {
+//     var op = 1;  // initial opacity
+//     var timer = setInterval(function () {
+//         if (op <= 0.1){
+//             clearInterval(timer);
+//             element.style.display = 'none';
+//         }
+//         element.style.opacity = op;
+//         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+//         op -= op * 0.1;
+//     }, 50);
+// }
+
